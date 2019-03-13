533935f8a322c7825e5f3816493003e9c4fb31be
k3r1ck <frukerickjeff@gmail.com>
2019-03-13 02:21:51 +0100

    Definitio of database migrations

---
+++ database/migrations/2019_03_12_200837_create_api_key_access_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApiKeyAccessEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_key_access_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('api_key_id');
+            $table->ipAddress('ip_address');
+            $table->text('url');
+            $table->timestamps();
+            $table->index('ip_address');
+            $table->foreign('api_key_id')
+                    ->references('id')->on('api_keys')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_key_access_events');
+    }
+}
